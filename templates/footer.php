d367aa241da0bb4062a62d3ac5601920baa72fc8
Ben <59772495+killiani@users.noreply.github.com>
2020-01-15 15:58:58 +0100

    footer.php language optimization

--- templates/footer.php
+++ templates/footer.php
@@ -1,35 +1,31 @@
+<?php
+if ($GLOBALS['TL_LANGUAGE'] === 'de') {
+    $phone = 'Telefon';
+    $country = '';
+} elseif ($GLOBALS['TL_LANGUAGE'] === 'en') {
+    $phone = 'Phone';
+    $country = 'GERMANY';
+};
+?>
+
 <div class="footer-logo">
     <img src="files/images/layout/logo-white.svg">
 </div>
 <div class="footer-wrapper">
-    <nav class="footer-navigation">
-        <ul>
-            <li>
-                <a class="footer-link" href="home.html">Startseite</a>
-            </li>
-            <li>
-                <a class="footer-link" href="kontakt.html">Kontakt</a>
-            </li>
-            <li>
-                <a class="footer-link" href="datenschutz.html">Datenschutz</a>
-            </li>
-            <li>
-                <a class="footer-link" href="impressum.html">Impressum</a>
-            </li>
-        </ul>
-    </nav>
+    {{insert_module::9}}
     <div class="footer-info">
         <div class="footer-address">
             <div class="footer-icon"><img src="files/images/layout/address.svg"></div>
             <div class="footer-text">
                 pontes pabuli GmbH<br>
                 Harkortstraße 8<br>
-                04107 Leipzig
+                04107 Leipzig<br>
+                <?= $country ?>
             </div>
         </div>
         <div class="footer-phone">
             <div class="footer-icon"><img src="files/images/layout/phone.svg"></div>
-            <div class="footer-text"><a href="tel:+491736586018"><span class="info-desktop">Telefon&nbsp;&nbsp;&nbsp;</span>+49 172 340 10 14</a></div>
+            <div class="footer-text"><a href="tel:+491736586018"><span class="info-desktop"><?= $phone; ?>&nbsp;&nbsp;&nbsp;</span>+49 172 340 10 14</a></div>
         </div>
         <div class="footer-mail">
             <div class="footer-icon"><img src="files/images/layout/mail.svg"></div>
@@ -37,3 +33,4 @@
         </div>
     </div>
 </div>
+
